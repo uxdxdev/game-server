17e4d208d9c8342d390705d1c92f06414448f962
david.morton <david.morton@servicenow.com>
2022-01-02 19:47:48 +0000

    updates

--- src/index.js
+++ src/index.js
@@ -22,11 +22,7 @@ const events = {
   CONNECTION: 'connection',
   DISCONNECT: 'disconnect',
   CONNECTED: 'connected',
-  DRAW: 'draw',
-  INIT_CANVAS: 'init_canvas',
 };
-
-const currentCanvas = [];
 
 // API
 const app = express();
@@ -35,21 +31,14 @@ const app = express();
 app.use(function (req, res, next) {
   // only allow requests from the client URL
   res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET');
   next();
 });
 
 // server homepage
 app.get('/', (req, res) => {
-  res.send(
-    `<div>${getNumberOfConnectedClients()} clients connected</div><a href="${
-      process.env.CLIENT_URL
-    }">Go to client</a>`
-  );
+  res.send(`<div>${getNumberOfConnectedClients()} clients connected</div><a href="${process.env.CLIENT_URL}">Go to client</a>`);
 });
 
 // health check API
@@ -96,9 +85,7 @@ io.use(async (socket, next) => {
   if (token && isAuthenticated) {
     next();
   } else {
-    next(
-      new Error('You are not authorised to make a connection to this server')
-    );
+    next(new Error('You are not authorised to make a connection to this server'));
   }
 });
 
@@ -107,26 +94,13 @@ const getNumberOfConnectedClients = () => {
 };
 
 io.on(events.CONNECTION, (client) => {
-  console.log(
-    `User ${client.handshake.auth.userId} connected on socket ${
-      client.id
-    }, there are currently ${getNumberOfConnectedClients()} users connected`
-  );
+  console.log(`User ${client.handshake.auth.userId} connected on socket ${client.id}, there are currently ${getNumberOfConnectedClients()} users connected`);
 
   // when a client connects send them a notification
   client.emit(events.CONNECTED, client.id);
 
-  // send the current state of the canvas
-  client.emit(events.INIT_CANVAS, currentCanvas);
-
-  // when the server receives a 'draw' event from a client
-  // broadcast the data to all connected clients
-  client.on(events.DRAW, (data) => {
-    // save any update to the canvas state
-    currentCanvas.push(data);
-
-    // broadcast to connected clients
-    io.sockets.emit(events.DRAW, data);
+  client.on('player_update', (data) => {
+    io.sockets.emit('player_update', data);
   });
 
   client.once(events.DISCONNECT, () => {
